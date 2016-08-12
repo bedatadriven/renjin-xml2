558ed06ea339ac5f09e11b78ed86682b5f9ced3b
Maarten-Jan Kallen <mj@bedatadriven.com>
2016-08-12 18:48:37 +0200

    Added a print method for an xml_nodeset

--- src/main/R/classes.R
+++ src/main/R/classes.R
@@ -12,3 +12,10 @@ print.xml_document <- function(x) {
     cat("{xml_document}\n")
     cat(XmlDocumentParser$node_format(x$node), "\n")
 }
+
+print.xml_nodeset <- function(x) {
+    cat("{xml_nodeset (", length(x), ")}\n", sep = "")
+    for (i in seq_along(x)) {
+        cat("[", i, "] ", XmlDocumentParser$node_format(x[[i]]$node), "\n", sep = "")
+    }
+}
